b0d1d5083cb86574878ffbfdfcd21e87d02f20ac
RyomaArai <RyomaArai@RyomaArai-no-MacBook-Air.local>
2015-03-30 18:15:17 +0900

    TlServletの画面遷移まで、正常に動作することを確認しました

--- Products/BookPhrase/src/servlet/TLServlet.java
+++ Products/BookPhrase/src/servlet/TLServlet.java
@@ -1,7 +1,27 @@
 package servlet;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by RyomaArai on 15/03/29.
  */
-public class TLServlet {
+
+@WebServlet("/TlServlet")
+public class TlServlet extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/timeline.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    public void doPost() {
+
+    }
 }
