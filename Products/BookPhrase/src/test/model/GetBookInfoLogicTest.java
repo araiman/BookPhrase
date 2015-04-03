a5608a68c998990c81ad094ce969607e303db27f
RyomaArai <RyomaArai@RyomaArai-no-MacBook-Air.local>
2015-04-03 17:27:22 +0900

    test

---
+++ Products/BookPhrase/src/test/model/GetBookInfoLogicTest.java
@@ -0,0 +1,24 @@
+package test.model;
+
+import model.GetBookInfoLogic;
+
+import java.net.HttpURLConnection;
+
+/**
+ * Created by RyomaArai on 15/04/03.
+ */
+public class GetBookInfoLogicTest {
+    public static void main (String[] args) {
+        GetBookInfoLogic getBookInfoLogic = new GetBookInfoLogic();
+        /*
+        HttpURLConnection connection = getBookInfoLogic.execute("4344423046");
+
+        if (connection != null) {
+            System.out.println(connection);
+            System.out.println("テストに成功しました");
+        } else {
+            System.out.println("テストに失敗しました");
+        }
+        */
+    }
+}
